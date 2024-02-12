5cd12818739e87d26b3d6bdf18996d2d5bfc8354
axshaykp <admin@akshaykp.in>
2024-02-12 19:17:20 +0530

    random wallpaper script added

---
+++ bin/random_wall.sh
@@ -0,0 +1,15 @@
+#!/bin/env bash
+
+wallpaper_folder=~/Pictures/Wallpapers
+
+while true; do
+    wallpaper_location="$(ls "$wallpaper_folder" | shuf -n 1)"
+    
+    if [[ -f $wallpaper_folder/$wallpaper_location ]]; then
+        swww img "$wallpaper_folder/$wallpaper_location" --transition-fps 120 --transition-type any --transition-duration 3
+    else
+        exit 1
+    fi
+
+    sleep 60  # Sleep for 10 minutes before changing wallpaper again
+done
